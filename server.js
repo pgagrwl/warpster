93c0d51eb9c71ce9be2f1abc399fc90252bfd924
pgagrwl <pgagrwl@gmail.com>
2024-03-24 10:12:35 +0530

    engagement rank added

--- server.js
+++ server.js
@@ -41,6 +41,38 @@ app.post("/api/follower-rank", async (req, res) => {
   }
 });
 
+//Engagement-rank
+app.post("/api/engagement-rank", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://graph.cast.k3l.io/scores/global/engagement/handles",
+      req.body,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+
+    res.json(response.data.result[0].rank);
+  } catch (error) {
+    console.error(error);
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      res.status(error.response.status).json(error.response.data);
+    } else if (error.request) {
+      console.log(error.request);
+      res.status(500).send("No response received from the external API");
+    } else {
+      console.log("Error", error.message);
+      res.status(500).send("Error making the request");
+    }
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
