b768619e674954cbc93c009b8b3fcf29001f8342
pgagrwl <pgagrwl@gmail.com>
2024-03-24 11:35:52 +0530

    apis added for extended network suggestion

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 //Follower-rank
-app.post("/api/follower-rank", async (req, res) => {
+app.post("/api/following-rank", async (req, res) => {
   try {
     const response = await axios.post(
       "https://graph.cast.k3l.io/scores/global/following/handles",
@@ -23,7 +23,7 @@ app.post("/api/follower-rank", async (req, res) => {
       }
     );
 
-    res.json(response.data.result[0].rank);
+    res.json(response.data);
   } catch (error) {
     console.error(error);
     if (error.response) {
@@ -55,7 +55,69 @@ app.post("/api/engagement-rank", async (req, res) => {
       }
     );
 
-    res.json(response.data.result[0].rank);
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      res.status(error.response.status).json(error.response.data);
+    } else if (error.request) {
+      console.log(error.request);
+      res.status(500).send("No response received from the external API");
+    } else {
+      console.log("Error", error.message);
+      res.status(500).send("Error making the request");
+    }
+  }
+});
+
+app.post("/api/suggestion-extended-following", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://graph.cast.k3l.io/scores/personalized/following/handles?k=2&limit=10",
+      req.body,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      res.status(error.response.status).json(error.response.data);
+    } else if (error.request) {
+      console.log(error.request);
+      res.status(500).send("No response received from the external API");
+    } else {
+      console.log("Error", error.message);
+      res.status(500).send("Error making the request");
+    }
+  }
+});
+
+app.post("/api/suggestion-extended-engagement", async (req, res) => {
+  try {
+    const response = await axios.post(
+      "https://graph.cast.k3l.io/scores/personalized/engagement/handles?k=2&limit=10",
+      req.body,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+
+    res.json(response.data);
   } catch (error) {
     console.error(error);
     if (error.response) {
